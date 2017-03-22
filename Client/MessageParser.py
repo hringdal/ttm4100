268cdbf703f4b0ec4e8834b10de3a750f3cedff4
hringdal <hakon.ringdal@gmail.com>
2017-03-22 19:16:00 +0100

    Initial Commit

---
+++ Client/MessageParser.py
@@ -0,0 +1,32 @@
+import json
+
+class MessageParser():
+    def __init__(self):
+
+        self.possible_responses = {
+            'error': self.parse_error,
+            'info': self.parse_info,
+            'message': self.parse_message,
+            'history': self.parse_history
+        }
+
+    def parse(self, payload):
+        payload = json.loads(payload) #load payload as json object
+
+        if payload['response'] in self.possible_responses:
+            return self.possible_responses[payload['response']](payload)
+        else:
+            return "Invalid server response received"
+
+    def parse_error(self, payload):
+        return payload.get('timestamp') + " Error: " + payload.get('content')
+    def parse_info(self, payload):
+        pass
+    def parse_message(self, payload):
+        pass
+    def parse_history(self, payload):
+        all_history = payload.get('content')
+        history = ""
+        for line in all_history:
+            history += self.parse_message(json.loads(line)) + "\n"
+        return history
---
+++ Client/MessageReceiver.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from threading import Thread
+import sys
+
+class MessageReceiver(Thread):
+    """
+    This is the message receiver class. The class inherits Thread, something that
+    is necessary to make the MessageReceiver start a new thread, and it allows
+    the chat client to both send and receive messages at the same time
+    """
+
+    def __init__(self, client, connection):
+        """
+        This method is executed when creating a new MessageReceiver object
+        """
+
+        super(MessageReceiver, self).__init__()
+
+        # Flag to run thread as a deamon
+        self.daemon = True
+
+        # TODO: Finish initialization of MessageReceiver
+
+        self.client = client
+        self.connection = connection
+
+
+    def run(self):
+        while True:
+            data = self.connection.recv(1024)
+            if data:
+                for message in data.split("\n"):
+                    if message:
+                        self.client.receive_message(message)
+            else:
+                sys.exit()
